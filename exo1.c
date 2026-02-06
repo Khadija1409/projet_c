0ff07e59de611b6f531de21669e23d1941e470c4
Khadija14 <Khadykandji2004@gmail.com>
2026-02-06 11:17:27 +0000

    n entier negatif

---
+++ exo1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    intx ,n;
+    do{
+    printf("saisir n entiers:");
+    scanf("%d",&n);
+    }while(n<0);
+    for(int i=0;i<n,i++){
+        do{
+            printf("veuillez saisir un entier negatif:");
+        scanf("%d",x);
+        }while(x>0);
+        printf("le nombre que tu as saisi est:%d",x);
+    }
+return 0;
+}
